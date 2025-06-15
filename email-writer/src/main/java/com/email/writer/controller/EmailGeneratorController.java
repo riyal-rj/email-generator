8b7eda11a337c9c212eda90772c710e550379fc9
Ritankar Jana <ritankar.jana.official@gmail.com>
2025-06-15 20:21:55 +0530

    changed CORS origin backend

--- email-writer/src/main/java/com/email/writer/controller/EmailGeneratorController.java
+++ email-writer/src/main/java/com/email/writer/controller/EmailGeneratorController.java
@@ -3,13 +3,19 @@ package com.email.writer.controller;
 import com.email.writer.dto.EmailRequestDTO;
 import com.email.writer.service.EmailGeneratorService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/email")
 @AllArgsConstructor
-@CrossOrigin(origins="http://localhost:5173", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS})
+@Slf4j
+@CrossOrigin(
+        origins = {"http://localhost:5173", "https://mail.google.com", "chrome-extension://*"},
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS},
+        allowedHeaders = {"Content-Type", "Authorization", "Accept", "Origin", "X-Requested-With"}
+)
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
